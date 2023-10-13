300f51d17293ca46400bbe543e82d0faf30a6afc
LucasMoragas <moragaslucas08@gmail.com>
2023-10-13 15:45:37 -0300

    Refactoring the code

--- project01/app.js
+++ project01/app.js
@@ -1,18 +1,23 @@
 var express = require('express')
+var msg = require('./test_modules')
+
 var app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/tech', function(req, res){
-    res.render('./section/tech.ejs')
+app.get('/', function(req, res){
+    res.render('./home/index.ejs')
 })
 
+app.get('/news', function(req, res){
+    res.render('./noticias/noticias.ejs')
+})
 
-app.get('/', function(req, res){
-    res.send('<html><body>News portal with Node!</body></html>')
+app.get('/form_add_news', function(req, res){
+    res.render('./admin/form_add_news.ejs')
 })
 
 
 app.listen(3000, function(){
-    console.log("Server running")
+    console.log(msg())
 })
---
+++ project01/test_modules.js
@@ -0,0 +1,4 @@
+module.exports = function(){
+    var msg = "This module contais just one string"
+    return msg;
+}
